3eae64699a053809a69b928c58232e9e68de812b
swadhinlenka <1985swadhin@gmail.com>
2018-12-25 01:35:18 +0530

    some new file added and some modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
-import { CourcesCompotent } from './cources.component';
+import { CoursesService } from './courses.service';
+import { CoursesCompotent } from './courses.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourcesCompotent
+    CoursesCompotent,
+    CourseComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    CoursesService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cources.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector : 'cources',
-    template : '<h2>Cources</h2>' 
-})
-export class CourcesCompotent{
-
-}
